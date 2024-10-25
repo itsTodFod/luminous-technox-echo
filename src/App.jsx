3f68c35857add6fe2ee093effa66bfaab2e27d4a
Avval Narang <avvalnnnnn@gmail.com>
2024-10-26 04:32:44 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold w-screen  text-blue-600">Luminous Techno-X</h1>
+    </div>
+  );
+}
+
+export default App;
